81473017f8d16297512d6225f1f532b44923d90d
noessler1 <felix.noessler@uni-potsdam.de>
2021-02-06 13:36:10 +0100

    first run

--- buds.py
+++ buds.py
@@ -1,4 +1,4 @@
-from skimage import io, filters, morphology, color, measure
+from skimage import io, filters, morphology, color, measure, img_as_float, feature
 from sklearn import decomposition, preprocessing, linear_model
 from matplotlib import cm, pyplot as plt
 import matplotlib
@@ -9,55 +9,59 @@ import glob
 
 def image_extraction(control_images=False):
     filenames = glob.glob("img/*.jpg")
-    scale_positions = [slice(0,3000), slice(650, 1500)], [slice(0, 3000), slice(0, 1000)]
-    buds_positions =  [slice(800, 3500), slice(1500, 3800)], [slice(1300, 5000), slice(990, 3800)]
+
+    scale_positions = np.tile([slice(2300, 4300), slice(500,1400)], (len(filenames),1))
+    buds_positions =  np.tile([slice(1600, 4700), slice(1320, 3000)], (len(filenames),1))
     data = pd.DataFrame()
     if control_images:
         matplotlib.use('Agg')
 
-    for filename, scale_position, buds_position in zip(filenames, scale_positions, buds_positions):
+    for filename in filenames:
 
         ### read the image
         img = io.imread(filename)
 
         ############################## scale
-        ### extract the image part with the scale
-        scale = img[scale_position[0], scale_position[1], 2]
-        scale_filename = 'control_img/scale_' + filename.split('/')[1]
-        #io.imsave(scale_filename, scale)
+        ### make binary
+        binary = img[:, :, 1] < filters.threshold_otsu(img[:, :, 1])
 
-        ### find a trehshold
-        scale_treshold = filters.threshold_otsu(scale)
+        ### remove small object
+        binary = morphology.remove_small_objects(binary, min_size=400000)
 
-        ### histogram with treshold
+        ### remove large objects
+        binary_label = measure.label(binary)
+        too_big = np.bincount(binary_label.ravel()) > 900000
+        too_big_mask = too_big[binary_label]
+        binary[too_big_mask] = 0
+
+        ### control image for scale
         if control_images:
-            _y, _x, _ = plt.hist(scale.ravel(), bins=256, color='tab:orange')
-            plt.plot([scale_treshold,scale_treshold], [0,np.max(_y)], '-', color='black')
-            scale_hist_filename = filename.split('/')[0] + '/data/hist_scale_' + filename.split('/')[1]
-            plt.savefig(scale_hist_filename)
-            plt.close()
+            scale = np.copy(img)
+            scale[np.logical_not(binary)] = scale[np.logical_not(binary)] * 0.4
+            scale_filename = 'control_img/scale_' + filename.split('/')[1]
+            io.imsave(scale_filename, scale)
 
-        ### make image binary
-        scale_binary = scale < scale_treshold
-
-        ### fill holes
-        scale_binary = scipy.ndimage.binary_fill_holes(scale_binary)
-
-        #### remove small objects
-        scale_binary = morphology.remove_small_objects(scale_binary, min_size=5000)
+        ### Get the coordinates of the scale
+        scale_props = measure.regionprops(measure.label(binary))
+        coordinates = scale_props[0].coords
+        y, x = coordinates[:, 0], coordinates[:, 1]
 
         ### calculate conversion factors (pixels -> length or area)
-        scale_area = np.sum(scale_binary) / 10  # scale 10cm^2 --> calculated with mm
-        scale_line = np.sqrt(np.sum(scale_binary) / 10)
+        scale_area = np.sum(binary) / 10  # scale 10cm^2 --> calculated with mm
+        scale_line = np.sqrt(np.sum(binary) / 10)
 
         ############################## buds
-        ### extract the image part with the buds
-        buds = img[buds_position[0], buds_position[1], 2]
-        buds_filename = 'control_img/buds_' + filename.split('/')[1]
-        #io.imsave(buds_filename, buds)
+        ### Extract the image part with buds
+        x_start = np.max(x)
+        x_end = x_start + np.int((binary.shape[1] - np.max(x)) * 0.8)
+        y_start = np.int(0.2 * binary.shape[0])
+        y_end = np.int(0.95 * binary.shape[0])
+
+        buds = img[y_start:y_end, x_start:x_end, 1]
 
         ### find a trehshold
         buds_treshold = filters.threshold_otsu(buds)
+        buds_treshold = buds_treshold * 0.8
 
         ### histogram with treshold
         if control_images:
@@ -115,7 +119,12 @@ def image_extraction(control_images=False):
         props.area = props.area / scale_area
         props.iloc[:, 0:2] = props.iloc[:, 0:2] / scale_line
         props['ratio'] = props.minor_axis_length / props.major_axis_length
-        props['name'] = filename.split('/')[1].split('.')[0]
+
+
+        img_name = filename.split('/')[1].split('.')[0]
+        props['name'] = img_name.split('-')[0]
+        props['location'] = img_name.split('-')[1]
+        props['img_no'] = img_name.split('-')[2]
         data = data.append(props)
 
         print('finished with ' + filename)
@@ -144,7 +153,7 @@ def plotting(data):
     plt.close()
 
 def calc_pca(data):
-    x = data.iloc[:,0:-1].values # remove name colums
+    x = data.iloc[:,0:-3].values # remove name colums
     x = preprocessing.StandardScaler().fit_transform(x)
     pca = decomposition.PCA()
     X = pca.fit_transform(x)
@@ -163,14 +172,14 @@ def calc_pca(data):
                  color=cm.Set1(i), label=i_species)
 
     plt.title('PCA')
-    plt.xlabel(f'PC1 ({np.round(variance_explained[0], 4) *100} %)')
-    plt.ylabel(f'PC2 ({np.round(variance_explained[1], 4) *100} %)')
+    plt.xlabel(f'PC1 ({np.round(variance_explained[0] * 100, 2)} %)')
+    plt.ylabel(f'PC2 ({np.round(variance_explained[1] * 100, 2)} %)')
     plt.legend()
     plt.savefig('graphics/pca.svg')
     plt.close()
 
 def logistic_regression(data):
-    x = data.iloc[:,0:-1].values
+    x = data.iloc[:,0:-3].values
     logreg = linear_model.LogisticRegression(solver='lbfgs')
     logreg.fit(x, data.name)
     z = logreg.predict(x)
@@ -193,15 +202,16 @@ def logistic_regression(data):
     plt.ylabel('Length cm')
     plt.xlabel('Area cm2')
     plt.legend()
+    plt.savefig('graphics/log_regression.svg')
     plt.show()
 
 
 if __name__ == '__main__':
-    #stats = image_extraction(control_images=False)
-    #save_as_csv(stats)
+    #stats = image_extraction(control_images=True)
 
+    #save_as_csv(stats)
     stats = load_csv()
-    #plotting(stats)
+    plotting(stats)
     logistic_regression(stats)
     calc_pca(stats)
     #print(stats)
